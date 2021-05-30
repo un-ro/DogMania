a869b0aa06a4b332d3c9b27a60f14f2d9d081839
Unero <ramadhanbhg@gmail.com>
2021-05-30 08:58:53 +0700

    :tada: Start working on Detail Page

---
+++ app/src/main/java/com/unero/dogmania/ui/detail/DetailFragment.kt
@@ -0,0 +1,29 @@
+package com.unero.dogmania.ui.detail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.unero.dogmania.databinding.FragmentDetailBinding
+import org.koin.android.ext.android.inject
+
+class DetailFragment : Fragment() {
+
+    private val viewModel: DetailViewModel by inject()
+    private var _binding: FragmentDetailBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
---
+++ app/src/main/java/com/unero/dogmania/ui/detail/DetailViewModel.kt
@@ -0,0 +1,7 @@
+package com.unero.dogmania.ui.detail
+
+import androidx.lifecycle.ViewModel
+import com.unero.dogmania.core.domain.usecase.DogUseCase
+
+class DetailViewModel(private val useCase: DogUseCase) : ViewModel() {
+}
