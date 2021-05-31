10ecdb9c5b2b3d348bf956ae1a9512ce4515a0b3
Unero <ramadhanbhg@gmail.com>
2021-05-31 19:21:09 +0700

    :boom: Delete unused module

--- favorite/src/main/java/com/unero/dogmania/favorite/FavoriteViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.unero.dogmania.favorite
-
-import androidx.lifecycle.ViewModel
-import com.unero.dogmania.core.domain.usecase.DogUseCase
-
-class FavoriteViewModel(useCase: DogUseCase): ViewModel() {
-}
