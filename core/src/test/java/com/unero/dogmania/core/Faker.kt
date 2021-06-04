9cc6619817f41564b02a5580ebb6464974981c00
Unero <ramadhanbhg@gmail.com>
2021-06-04 10:46:03 +0700

    :white_check_mark: Unit Test

---
+++ core/src/test/java/com/unero/dogmania/core/Faker.kt
@@ -0,0 +1,23 @@
+package com.unero.dogmania.core
+
+import com.unero.dogmania.core.domain.model.Dog
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+object Faker {
+    fun dummyDogs(): Flow<List<Dog>> {
+        return flow {
+            listOf(
+                dog, dog, dog, dog, dog
+            )
+        }
+    }
+
+    val dog = Dog(
+        0,
+        "https://images.dog.ceo/breeds/hound-afghan/n02088094_10982.jpg",
+        "",
+        "",
+        false
+    )
+}
